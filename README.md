e72ee1fe941e1fc9154cfa7cb3ee08d5774b00ca
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-11-18 13:38:48 +0600

    solved all the problem and working on some bug fixing

--- README.md
+++ README.md
@@ -1,31 +1,50 @@
-# Interview Questions - Blog Task
-
-## Blog Post (in Bengali): Answering any 2 of the questions below in below:
+# Blog Post:
 
 ---
 
 ### 1.What are some differences between interfaces and types in TypeScript?
 
 **_Answer:_**
+**Interface** (ইন্টারফেজ) এবং **types** (টাইপস) দুইটাই টাইপ ডিক্লার করার জন্য টাইপস্ক্রিপ্টে ইউজ করা হয়। কিন্তু এদের ইউজ করার প্রোসেস এবং ইউজ কেস আলাদা। নিচে interface এবং type এর মধ্যের ডিফারেন্স দেয়া হলোঃ
+
+**১। Declaration :** interface এবং type দুইটা ডিক্ল্যার করার প্রোসেস আলাদা ।
+interface ডিক্ল্যার করা হয় --> interface IUser {id:number; name:string; age:number}
+
+type ডিক্ল্যার করা হয় --> type TUser={id:number; name:string; age:number}
+
+**২। Extension :** interface এবং type দুইটাই এক্সটেন্ড করা যায় বাট এক্সটেন্ড করার প্রোসেস দুইটার আলাদা ।
+interface এক্সটেন্ড করার জন্য extends কিওয়ার্ড ইউজ করা হয় ।
+example : interface A {x:number};
+interface B extends A {y:string}
+
+type এক্সটেন্ড করার জন্য & ইউজ করা হয় ।
+example: type A= {x:number};
+type B= A & {y:string};
+
+**৩। Use Case:** interface মূলত অবজেক্টের স্ট্রাকচার ডিফাইন করতে ইউজ করা হয় লাইক Object , class etc.
+type আরও ফ্লেক্সিবল এবং এটা প্রায় সব টাইপের ভ্যারিয়েবলে ইউজ করা যায়। লাইক Union , primitive type , tuple etc.
+
+---
 
 ## What is the use of the keyof keyword in TypeScript? Provide an example.
 
-**_Answer:_**
+**_Answer:_** **keyof** হলো টাইপস্ক্রিপ্টের একটি অপারেটর । এটা যে কোন অবজেক্ট টাইপের সব key গুলোকে একটি ইউনিউন টাইপ হিসেবে রিটার্ন করে । যখন কোন অবজেক্টের key গুলোকে টাইপ হিসেবে ইউজ করতে চাই তখন keyof অপারেটর ইউজ করা হয়।
 
----
+**Example:** 1.
+type Vehicles={bike:string; car:string; cng:string};
+type MyVehicle = "bike"|"car"|"cng";
+type MyVehicle1= keyof Vehicles;
 
-### Explain the difference between any, unknown, and never types in TypeScript.
+এখানে টাইপ MyVehicle এবং MyVehicle1 দুইটাই সেম।
 
-**_Answer:_**
+    2.
 
----
+interface User {
+id:number;
+name:string;
+email:string;
+}
 
-### What is the use of enums in TypeScript? Provide an example of a numeric and string enum.
+type UserKeys=keyof User;
 
-**_Answer:_**
-
----
-
-### Provide an example of using union and intersection types in TypeScript.
-
-**_Answer:_**
+এখানে UserKeys টাইপ হবে "id"।"name"।"email"
--- src/solution.ts
+++ src/solution.ts
@@ -1,7 +1,3 @@
-// initialized the assignment and readme file.
-
-// problem 1
-
 function formatValue(
   value: string | number | boolean,
 ): string | number | boolean {
@@ -14,24 +10,15 @@ function formatValue(
   }
 }
 
-console.log(formatValue("hello"));
-console.log(formatValue(5));
-console.log(formatValue(true));
-
-// Problem 2
-
-function getLength(value: string | Array<number | string | boolean>): number {
+function getLength(value: string | Array<unknown>): number {
   if (typeof value === "string") {
     return value.length;
-  } else {
+  } else if (Array.isArray(value)) {
     return value.length;
+  } else {
+    throw new Error("Value must be a string or an array");
   }
 }
-
-console.log(getLength("typescript"));
-console.log(getLength([10, 20, 30, 40, "dhsj", true]));
-
-// problem 3
 
 class Person {
   name: string;
@@ -41,12 +28,138 @@ class Person {
     this.age = age;
   }
   getDetails(): string {
-    return `Name: ${this.name}, Age: ${this.age}`;
+    return `'Name: ${this.name}, Age: ${this.age}'`;
   }
 }
 
-const person1 = new Person("John Doe", 30);
-console.log(person1.getDetails());
+function filterByRating(
+  arr: Array<{
+    title: string;
+    rating: number;
+  }>,
+): Array<{
+  title: string;
+  rating: number;
+}> {
+  const newArr: Array<{
+    title: string;
+    rating: number;
+  }> = arr.filter((value) => {
+    if (value.rating < 0 || value.rating > 5) {
+      throw new Error(
+        `Invalid rating: ${value.rating}. Rating must be between 0-5.`,
+      );
+    }
+    return value.rating >= 4;
+  });
+  return newArr;
+}
 
-const person2 = new Person("Alice", 25);
-console.log(person2.getDetails());
+function filterActiveUsers(
+  users: Array<{
+    id: number;
+    name: string;
+    email: string;
+    isActive: boolean;
+  }>,
+): Array<{
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+}> {
+  const newArray: Array<{
+    id: number;
+    name: string;
+    email: string;
+    isActive: boolean;
+  }> = [];
+  for (let arr of users) {
+    if (arr.isActive === true) {
+      newArray.push(arr);
+    }
+  }
+
+  return newArray;
+}
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(value: Book): void {
+  console.log(
+    `Title: ${value.title}, Author: ${value.author}, Published: ${
+      value.publishedYear
+    }, Available: ${value.isAvailable ? "Yes" : "No"}`,
+  );
+}
+
+function getUniqueValues(
+  arr1: Array<string | number>,
+  arr2: Array<string | number>,
+): Array<string | number> {
+  const combinedArr: Array<string | number> = [];
+  for (let value of arr1) {
+    combinedArr[combinedArr.length] = value;
+  }
+  for (let value of arr2) {
+    combinedArr[combinedArr.length] = value;
+  }
+
+  let newArray: Array<string | number> = [];
+
+  for (const value of combinedArr) {
+    let check: boolean = false;
+    for (let i = 0; i < newArray.length; i++) {
+      if (value === newArray[i]) {
+        check = true;
+      }
+    }
+    if (!check) {
+      newArray[newArray.length] = value;
+    }
+  }
+
+  return newArray;
+}
+
+function calculateTotalPrice(
+  products: Array<{
+    name: string;
+    price: number;
+    quantity: number;
+    discount?: number;
+  }>,
+): number {
+  const calculatedPrice = products.reduce(
+    (
+      acc: number,
+      curr: {
+        name: string;
+        price: number;
+        quantity: number;
+        discount?: number;
+      },
+    ): number => {
+      if (curr.discount !== undefined) {
+        if (curr.discount < 0 || curr.discount > 100) {
+          throw new Error(`Invalid Discount: ${curr.discount}. Must be 0-100.`);
+        }
+      }
+
+      const totalPrice: number = curr.price * curr.quantity;
+      let totalDiscount: number = 0;
+      if (curr.discount !== undefined) {
+        totalDiscount = totalPrice * (curr["discount"] / 100);
+      }
+      const finalPrice = totalPrice - totalDiscount;
+      return acc + finalPrice;
+    },
+    0,
+  );
+  return calculatedPrice;
+}
